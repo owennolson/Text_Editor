e02a2fa5a3430ce27ef82958d3ea979ae5d7d3b4
abc <abc@example.com>
2023-06-03 13:09:30 -0400

    had to make a new repo because the tutor thought my file dtructure was wrong and thats why it was not working and she was half right but there was alo problems with my putDb and getDb that I have now fixed

---
+++ client/src/js/database.js
@@ -0,0 +1,34 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({jate: content });
+  const result = await request;
+  console.log('Content added to the database', result);
+};
+
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('Content added to the database', result);
+};
+
+initdb();
+
